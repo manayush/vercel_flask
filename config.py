3494794533934625b8063fe7c9e520d4ea15c472
Ayush <ag275490@gmail.com>
2024-09-04 14:45:21 +0530

    Adding Files

--- config.py
+++ config.py
@@ -2,8 +2,9 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY' , 'a_very_secret_key')
-    SQLALCHEMY_DATABASE_URI = ('postgresql://postgres:Ayush%40123@localhost:5432/new') 
-    # or 'postgresql://user:Zt5wYorpVSKXzV96Y9Y7KssyrwsUQ2yH@dpg-cqsurn5umphs73c49ha0-a.oregon-postgres.render.com/flask_contacts_db'
+    #SQLALCHEMY_DATABASE_URI = ('postgresql://postgres:Ayush%40123@localhost:5432/new') 
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL','postgresql://default:jQsnbc7Go3kh@ep-tiny-unit-a1my71aw-pooler.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require') 
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') 
     # or 'a_very_secret_jwt_key'
