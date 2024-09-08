907ead68bba5d233aa362843b0c2124dea0fe720
Ayush <ag275490@gmail.com>
2024-09-08 19:01:10 +0530

    initial commit

--- app.py
+++ app.py
@@ -3,9 +3,21 @@ from flask import Flask
 from config import Config
 from models import  db, bcrypt
 from flask_migrate import Migrate
-#from flask_sqlalchemy import SQLAlchemy
+
 from flask_jwt_extended import JWTManager
 
+
+
+from http.server import BaseHTTPRequestHandler
+ 
+class handler(BaseHTTPRequestHandler):
+ 
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write('Hello, world!'.encode('utf-8'))
+        return
 #db = SQLAlchemy()
 #migrate = Migrate()
 #bcrypt = Bcrypt()
@@ -30,7 +42,7 @@ def create_app(config_class=Config):
     return app
 
 # Uncomment and use this block to run the app directly
-# if __name__ == "__main__":
-#     app = create_app()
-#     port = int(os.environ.get('PORT', 5000))
-#     app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    app = create_app()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(port=port)
--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY' , 'a_very_secret_key')
+    SECRET_KEY = os.environ.get('SECRET_KEY' , 'y')
     #SQLALCHEMY_DATABASE_URI = ('postgresql://postgres:Ayush%40123@localhost:5432/new') 
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL','postgresql://default:jQsnbc7Go3kh@ep-tiny-unit-a1my71aw-pooler.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require') 
 
